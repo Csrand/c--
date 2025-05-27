6c2d3723d121ec9d9a360ba7d3267af4149d4054
chrystiansenna@gmail.com <chrystiansenna@gmail.com>
2025-05-26 21:59:10 -0300

    atualizando essa bomba aq

---
+++ ds/stack.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+#define MAX 100
+
+struct Produto{
+    int produto;
+    string descricao;
+    int qtde;
+    float preco;
+};
+
+struct Pilha{
+    Produto Prod[M];
+    int inicio;
+    int qtdeTotal;
+    float valorTotal;
+};
+
+struct Fila{
+    Produto Prod[MAX];``
+    int inicio;
+    int fim;
+    int qtdeTotal;
+    float valorTotal;
+  
+};
+void init
+int main(){
+  
+  return 0;
+}
+
